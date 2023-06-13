39784c6995486b6f4d54d703877b4a9ef07cad85
Juan Jose Rendon <juanjoserendon13@outlook.com>
2023-06-13 14:17:55 -0500

    add openai helpers

---
+++ src/helpers/get_completion_llm.py
@@ -0,0 +1,18 @@
+# Helper function to use prompts and look at the generated outputs
+def get_completion(openai, prompt, model="gpt-3.5-turbo", temperature=0):
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=temperature, # this is the degree of randomness of the model's output
+    )
+    return response.choices[0].message["content"]
+
+def get_completion_from_messages(openai, messages, model="gpt-3.5-turbo", temperature=0):
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=temperature, # this is the degree of randomness of the model's output
+    )
+#     print(str(response.choices[0].message))
+    return response.choices[0].message["content"]
---
+++ src/helpers/setup_openai.py
@@ -0,0 +1,8 @@
+import openai
+import os
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv())
+
+openai.api_key  = os.getenv('OPENAI_API_KEY')
+
+OPENAI_MODULE = openai
